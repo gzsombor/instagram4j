c9d4cb1b458bce16379042c199ac3f5098377857
Zsombor Gegesy <gzsombor@gmail.com>
2020-01-15 01:06:03 +0100

    InstagramFeedItem could be an InstagramItem as well

--- src/main/java/org/brunocvcunha/instagram4j/requests/payload/InstagramFeedItem.java
+++ src/main/java/org/brunocvcunha/instagram4j/requests/payload/InstagramFeedItem.java
@@ -31,33 +31,16 @@ import java.util.Map;
 @Getter
 @Setter
 @ToString(callSuper = true)
-public class InstagramFeedItem {
+public class InstagramFeedItem extends InstagramItem {
 
-    public long taken_at;
-    public long pk;
-    public String id;
-    public long device_timestamp;
-    public int media_type;
-    public String code;
-    public String client_cache_key;
-    public int filter_type;
-    public boolean has_audio;
-    public double video_duration;
-    public Map<String, Object> attribution;
-    public List<ImageMeta> video_versions;
-    public ImageVersions image_versions2;
     public InstagramUserTagsContainer usertags;
     public FeedItemLocation location;
     public float lng;
     public float lat;
-    public int original_width;
-    public int original_height;
     public int view_count;
-    public InstagramUser user;
 
     public List<InstagramCarouselMediaItem> carousel_media;
     
-    public String organic_tracking_token;
     public int like_count;
     public List<String> top_likers;
     public List<InstagramUserSummary> likers;
@@ -69,13 +52,5 @@ public class InstagramFeedItem {
     public List<InstagramComment> preview_comments;
     public List<Object> comments;
     public int comment_count;
-    public InstagramComment caption;
 
-    public boolean can_viewer_reshare;
-    public boolean caption_is_edited;
-    public boolean photo_of_you;
-    public boolean comments_disabled;
-    public boolean can_viewer_save;
-    public boolean has_viewer_saved;
-    
 }
